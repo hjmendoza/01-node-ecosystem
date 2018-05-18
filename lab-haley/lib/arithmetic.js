5bbbc87fd89e9586938a5fc20329b2f0a5b1a6f4
Haley Mendoza <mendozahaley@gmail.com>
2018-05-17 21:41:24 -0700

    sub method functioning. tests passing locally.

--- lab-haley/lib/arithmetic.js
+++ lab-haley/lib/arithmetic.js
@@ -5,6 +5,13 @@ var arithmetic = {
     } 
     return (num1 + num2);
   },
+
+  sub: function(num1, num2){
+    if(typeof num1  !== 'number'|| typeof num2 !== 'number') {
+      return null;
+    } 
+    return (num2 - num1);
+  },
 };
 
 module.exports = arithmetic;
--- lab-haley/test/arithmetic.test.js
+++ lab-haley/test/arithmetic.test.js
@@ -1,12 +1,20 @@
 const arithmetic = require('../lib/arithmetic.js');
 
 describe('arithmetic module', () => {
-  
+
   it('should return null if either argument is a non-number', () => {
     expect(arithmetic.add()).toBeNull();
   });
 
   it('should return the sum of both numbers when passed numbers', () => {
     expect(arithmetic.add(1, 2)).toEqual(3);
+  });
+
+  it('should return null if either argument is a non-number', () => {
+    expect(arithmetic.sub()).toBeNull();
+  });
+
+  it('should return the sum of both numbers when passed numbers', () => {
+    expect(arithmetic.sub(1, 2)).toEqual(1);
   });
 });
